b28fc3cc4ae1219578289743346d4ef23d23ed6b
Anymore-build <1692028331@qq.com>
2024-10-13 23:27:42 +0800

    更新 settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -44,13 +44,10 @@ include(
     ":services:manager-service",
     ":services:xposed-service:interface",
     ":share:android",
-    ":share:java",
-    ":app",
-    ":libxposed"
+    ":share:java"
 )
 
 project(":core").projectDir = file("core/core")
-project(":libxposed").projectDir = file("libxposed/api")
 project(":hiddenapi:bridge").projectDir = file("core/hiddenapi/bridge")
 project(":hiddenapi:stubs").projectDir = file("core/hiddenapi/stubs")
 project(":services:daemon-service").projectDir = file("core/services/daemon-service")
